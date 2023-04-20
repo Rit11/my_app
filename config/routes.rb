ec46565adccb8ab25656bc0f72f26fcb4f927d76
Rit11 <33058899+Rit11@users.noreply.github.com>
2023-04-21 02:03:59 +0530

    about us page addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # root 'application#hello'
   root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'home', to: 'pages#home'
   resources :high_scores
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
