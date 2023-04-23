9a2abccafb0adfb5b7eca5cd389aadd4fc84de9c
Rit11 <33058899+Rit11@users.noreply.github.com>
2023-04-23 22:35:27 +0530

    including articles pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :articles
   # root 'application#hello'
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'home', to: 'pages#home'
+  get 'articles', to: 'articles#index'
+
   resources :high_scores
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
